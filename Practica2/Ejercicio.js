ceacc08f942e3a6c8844e1372f4e027db632abbd
RafaelBlazquez <rafael.blazquez@ikasle.egibide.org>
2021-11-05 13:20:11 +0100

    viernes

--- Practica2/Ejercicio.js
+++ Practica2/Ejercicio.js
@@ -5,37 +5,46 @@ var usuario = document.getElementById("dni");
 var lista = document.getElementById("numeros");
 var lis = document.getElementsByTagName("li");
 var tecladoGenerado = false;
+var cantidad =0;
 
 usuario.addEventListener("focusin",eliminarTeclado, true);
 
 function claveActiva(){
     if(!tecladoGenerado){
         generarTeclado()
+        clave.classList.add("seleccionado");
     }
 }
 function generarTeclado(){
-    let numeros = ["0","1","2","3","4","5","6","7","8","9"];
+    var numeros = ["0","1","2","3","4","5","6","7","8","9"];
     for(let x =0 ;x< 10; x++){
-        let posicionAzar = Math.floor(Math.random() * (numeros.length-1));
+        var posicionAzar = Math.floor(Math.random() * (numeros.length-1));
         let elemento = document.createElement("li");
         var boton = document.createElement("p");
         boton.setAttribute("class","numero");
         boton.setAttribute("id",numeros[posicionAzar]);
-        boton.addEventListener("click" ,escribirNumero(numeros[posicionAzar]) , true );
+        boton.addEventListener("click" ,escribirNumero, false );
         let contenido =document.createTextNode(numeros[posicionAzar]);
         elemento.appendChild(boton);
         boton.appendChild(contenido);
         lista.appendChild(elemento);
         numeros.splice(posicionAzar,1);
-        
+
     }
     tecladoGenerado=true;
 }
-function escribirNumero(numero){
-    if(clave.getAttribute("value") !=null)
-        clave.setAttribute("value", clave.getAttribute("value")+numero);
-    else{
-        clave.setAttribute("value", numero);
+function escribirNumero(){
+
+    if (cantidad<6){
+        console.log(cantidad);
+        if(clave.getAttribute("value") !=null){
+            clave.setAttribute("value", clave.getAttribute("value")+this.getAttribute("id"));
+            cantidad++;
+        }
+        else{
+            clave.setAttribute("value", this.getAttribute("id"));
+            cantidad++;
+        }
     }
 }
 function eliminarTeclado(){
